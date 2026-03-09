510bf2a3b21245b8d1391b1d77c3f69ec82f218e
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2026-03-09 13:25:28 +0100

    windows only .ico

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
+#include <QDebug>
+#include <QFile>
 #include <QGuiApplication >
+#include <QIcon>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
-    qputenv("QT_QUICK_CONTROLS_STYLE", "Material");
-    QGuiApplication  app(argc, argv);
+int main(int argc, char *argv[]) {
+  qputenv("QT_QUICK_CONTROLS_STYLE", "Material");
+  QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
+  QQmlApplicationEngine engine;
 
+  app.setWindowIcon(QIcon(":/qt/qml/com/Roboticus/ControlCenter/qml/assets/"
+                          "icons/Roboticus_icon.ico"));
 
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("com.Roboticus.ControlCenter", "Main");
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+      []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+  engine.loadFromModule("com.Roboticus.ControlCenter", "Main");
 
-    return app.exec();
+  return app.exec();
 }
